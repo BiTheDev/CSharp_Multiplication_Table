827d7c7ae348f1dd8a4e2cd4281bf518c3c91ca2
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-10-01 16:15:00 -0700

    push

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    int[,,] array3D = int[10,10,10]
+        //    {
+        //         for (int i = 1; i <= 10; i++)
+        //         {
+        //             for (int j = 1; j <= 10; j++)
+        //             {           
+        //                     System.Console.WriteLine(i * (j+j));
+        //             }
+        //         }
+        //     }
+        // int[,] Multiplication_Table = new int[10,10];
+        // for (int i = 1; i <= 10; i++)
+        // {
+        //     for (int j = 1; j <= 10; j++)
+        //     {
+        //         System.Console.Write(i * j + "\t");
+        //     }
+        //     System.Console.WriteLine();
+        // }
+        // }
+
+        int[,] Multiplication_Table = new int[10,10];
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Multiplication_Table[i-1,j-1] = i * j;
+                System.Console.Write(Multiplication_Table[i-1,j-1] + "\t");
+            }
+            System.Console.WriteLine();
+            
+        }
+        }
+
+    }
+}
